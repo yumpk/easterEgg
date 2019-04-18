10989cb846a18bfcd04ae7fbdcf786f9256e6c76
Damien Ruckebusch <ruckebusch.damien@gmail.com>
2019-04-18 18:52:47 +0200

    adding homepage component

--- easter-egg/src/App.js
+++ easter-egg/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Gallery from "./Components/Gallery";
 import "./App.css";
+import Homepage from "./Components/HomePage";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Gallery />
+        <Gallery />        
       </div>
     );
   }
