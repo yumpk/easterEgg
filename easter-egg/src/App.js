d7c87a1e6df93653b7500daf35f8d60bec466f4e
Sabine Robart <sabine.robart@protonmail.com>
2019-04-18 22:17:24 +0200

    Modifies logo import.

--- easter-egg/src/App.js
+++ easter-egg/src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import Gallery from "./Components/Gallery";
 import "./App.css";
 import Homepage from "./Components/HomePage";
-import logo from "./Images/logo.jpg";
+import logo from "./Images/AdopteUnOeuf.png";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <header className="header-app">
-      <img src={logo} className="app-logo" alt="logo" />
-      <p>AdopteUnOeuf.com</p>
-      </header>
-        <Gallery />        
+        <header className="header-app">
+          <img src={logo} className="app-logo" alt="logo" />
+          <p>AdopteUnOeuf.com</p>
+        </header>
+        <Gallery />
       </div>
     );
   }
