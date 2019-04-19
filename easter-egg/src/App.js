21e525af9f72583f64c8bb1de8bb97b2bdbf70cf
FredericMentel <frederic.mentel@gmail.com>
2019-04-19 09:43:43 +0200

    Add navigation function on App

--- easter-egg/src/App.js
+++ easter-egg/src/App.js
@@ -2,12 +2,39 @@ import React, { Component } from "react";
 import Gallery from "./Components/Gallery";
 import "./App.css";
 import Homepage from "./Components/HomePage";
+import SkillCards from "./Components/SkillCards";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      page : 'HomePage'
+    }
+
+  }
+
+  renderSwitch = (param) => {
+    switch (param) {
+      case 'Homepage':
+      return <Homepage />;
+        break;
+      case 'Gallery':
+      return <Gallery />
+        break;
+      case 'SkillCards':
+      return <SkillCards />
+        break;
+      default:
+        console.log('Sorry, we are out of ' + param + '.');
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Gallery />        
+
+       {this.renderSwitch(this.state.page)}
+
+       
       </div>
     );
   }
